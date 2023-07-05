2d14029a011db13a45bf228783331034922cb0a9
kuro <anhddse02700@gmail.com>
2023-07-05 16:08:51 +0900

    fix component export

--- web/frontend/components/index.ts
+++ web/frontend/components/index.ts
@@ -1,6 +1,5 @@
-import TicketTypes from './TicketType'
-import ScheduleForm from './ScheduleForm'
+export { default as TicketTypes } from './TicketType'
+export { default as ScheduleForm } from './ScheduleForm'
 
 export { ProductsCard } from './ProductsCard'
 export * from './providers'
-export { TicketTypes, ScheduleForm }
